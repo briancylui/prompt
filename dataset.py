1c4833e2c7029405c9f07c1cf845b63d2cedb3fd
Brian Lui <brianlui@stanford.edu>
2023-03-15 00:28:06 -0700

    Read in GradeSchoolMath (GSM8K) data

---
+++ dataset.py
@@ -0,0 +1,35 @@
+"""Constructs a dataset from GradeSchoolMath data."""
+
+from __future__ import annotations
+
+import dataclasses
+import json
+
+from typing import Optional
+
+
+_DEFAULT_TRAIN_PATH = 'data/train.jsonl'
+_DEFAULT_TEST_PATH = 'data/test.jsonl'
+
+
+class Dataset:
+    def __init__(self, path: str = _DEFAULT_TRAIN_PATH) -> None:
+        self.path = path
+        self.data = []
+
+        with open(self.path, 'r') as file:
+            for line in file:
+                self.data.append(json.loads(line.rstrip('\n|\r')))
+
+
+@dataclasses.dataclass
+class TrainTestSets:
+    train: Dataset
+    test: Dataset
+
+    def __init__(
+        self,
+        train: Optional[TrainTestSets] = None,
+        test: Optional[TrainTestSets] = None) -> None:
+        self.train = Dataset(_DEFAULT_TRAIN_PATH) if train is None else train
+        self.test = Dataset(_DEFAULT_TEST_PATH) if test is None else test
