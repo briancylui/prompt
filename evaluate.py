434363f57913a066df7ca9f3e49f48b4d2cfe47a
Brian Lui <brianlui@stanford.edu>
2023-03-17 00:36:09 -0700

    Update evaluator with enriched logic and broader audiences

--- evaluate.py
+++ evaluate.py
@@ -11,8 +11,10 @@ _QUESTION_KEY = 'question'
 _ANSWER_KEY = 'answer'
 _PROMPT_KEY = 'prompt'
 _RESPONSE_KEY = 'response'
+_NEW_PROMPT_KEY = 'new_prompt'
+_NEW_RESPONSE_KEY = 'new_response'
 
-_RESULT_DIRECTORY = 'results/arithmetics'
+_RESULT_DIRECTORY = 'results/arithmetics/answer_to_prompt_across_tags'
 
 
 def get_last_integer(response: str) -> Optional[int]:
@@ -33,8 +35,19 @@ class Evaluator:
     
     def evaluate(self) -> Mapping[str, Mapping[str, Union[int, float]]]:
         self.result = {}
-        
+
         for path in self.paths:
+            filename = path.split('/')[-1].rstrip('.jsonl')
+            model = filename.split('_')[0]
+            filename = filename.lstrip(model + '_')
+            tag = filename[:3]
+
+            unwanted_suffix = '_prompt_answer'
+            if filename.endswith(unwanted_suffix):
+                filename = filename.rstrip(unwanted_suffix)
+            helper_tag = filename[-3:] if not filename[-3:].isnumeric() else tag
+            n_shots = int(filename[4])
+
             records = []
             with open(path, 'r') as file:
                 for json_line in file:
@@ -46,23 +59,33 @@ class Evaluator:
             for record in records:
                 total_n += 1
                 answer = int(record[_ANSWER_KEY])
-                response = record[_RESPONSE_KEY]
+                response = record[_NEW_RESPONSE_KEY] if _NEW_RESPONSE_KEY in record else record[_RESPONSE_KEY]
                 model_answer = get_last_integer(response)
                 if model_answer is not None and answer == model_answer:
                     n_matches += 1
+
             self.result[path] = {
+                'model': model,
+                'tag': tag,
+                'helper_tag': helper_tag,
+                'n_shots': n_shots,
                 'accuracy': n_matches / total_n,
                 'n_correct': n_matches,
                 'total': total_n}
+
         return self.result
 
 
 if __name__ == '__main__':
-    save_path = os.path.join(_RESULT_DIRECTORY, 'summary.csv')
+    result_directory = _RESULT_DIRECTORY
+
+    save_path = os.path.join(result_directory, 'summary.csv')
     evaluator = Evaluator(
-        paths=sorted(glob.glob(_RESULT_DIRECTORY + '/*.jsonl'))
+        paths=sorted(glob.glob(result_directory + '/*.jsonl'))
     )
     results = evaluator.evaluate()
-    pd.DataFrame.from_dict(
-        results, orient='index'
-        ).reset_index().to_csv(save_path, index=False)
+    df = pd.DataFrame.from_dict(
+        results, orient='index')
+    df.index.name = 'source'
+    df = df.reset_index()
+    df.to_csv(save_path, index=False)
